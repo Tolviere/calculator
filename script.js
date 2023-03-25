1a7dfe25b036b30c054101f60b5504070a416e61
Matthias von Baeyer <79849301+Tolviere@users.noreply.github.com>
2023-03-25 05:31:20 -0400

    Add basic functions and css layout

---
+++ script.js
@@ -0,0 +1,36 @@
+let firstNum = 3;
+let secondNum = 5;
+let op = '+';
+
+console.log(operate(3, 5, op));
+
+function add(a, b) {
+    return +a + +b;
+}
+
+function subtract(a, b) {
+    return +a - +b;
+}
+
+function multiply(a, b) {
+    return +a * +b;
+}
+
+function divide(a, b) {
+    return +a / +b;
+}
+
+function operate(a, b, op) {
+    switch(op) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+        default:
+            return "oopsies";
+    }
+}
